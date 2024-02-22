a7ec8db10df58cd8756f8f561d9c7d18b3b00865
MamaneHassane <hassane.mamane017@gmail.com>
2024-02-22 23:06:36 +0100

    working on controllers

--- src/main/java/com/videogamesshop/vgs_package/controllers/CustomerController.java
+++ src/main/java/com/videogamesshop/vgs_package/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import com.videogamesshop.vgs_package.model.records.AuthenticateCustomerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.videogamesshop.vgs_package.service.CustomerService;
 
@@ -18,26 +19,31 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+    @PreAuthorize("hasAnyAuthority({'ADMIN,EMPLOYEE,MODERATOR'})")
     @GetMapping("/findAll")
     public ResponseEntity<List<Customer>> getAllCustomer(){
         List<Customer> customers= customerService.findAllCustomer();
         return new ResponseEntity<>(customers, HttpStatus.OK);
     }
+    @PreAuthorize("hasAnyAuthority({'ADMIN,EMPLOYEE,MODERATOR'})")
     @GetMapping("/findOne/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
         Customer customer = customerService.findCustomerById(id);
         return new ResponseEntity<>(customer,HttpStatus.OK);
     }
+    @PreAuthorize("hasAnyAuthority({'ADMIN,EMPLOYEE,MODERATOR'})")
     @PostMapping("/register")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         customerService.addCustomer(customer);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+    @PreAuthorize("hasAnyAuthority({'ADMIN,EMPLOYEE,MODERATOR'})")
     @PutMapping("/updateOne/{id}")
     public ResponseEntity<Customer> updateCustomer(@RequestBody Customer newCustomer,@PathVariable("id") Long id) {
         Customer customer = customerService.updateCustomerById(newCustomer,id);
         return new ResponseEntity<>(customer,HttpStatus.ACCEPTED);
     }
+    @PreAuthorize("hasAnyAuthority({'ADMIN,EMPLOYEE,MODERATOR'})")
     @DeleteMapping("/deleteOne/{id}")
     public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") Long id) {
         customerService.deleteCustomerById(id);
